f22022cf90c682ddacade7612294259372fdd45d
ina <wenulife@gmail.com>
2023-03-27 12:28:38 +0300

    deleted first 2 columns

--- data_formatter.py
+++ data_formatter.py
@@ -20,17 +20,29 @@ class DataFormatter:
                 full_file_name = os.path.join(root, file_name)
                 os.rename(full_file_name, f"{full_file_name}.csv")
 
-    def reformat_files(self):
+    def delete_first_2_columns(self):
         for root, dir_names, file_names in os.walk(self.files_dir):
             for file_name in file_names:
                 full_file_name = os.path.join(root, file_name)
-                data_frame = pd.read_csv(full_file_name)
-                data_frame.drop('Source Url', axis=1, inplace=True)
-                print(data_frame)
+                df = pd.read_csv(full_file_name)
+                df.drop('Source Url', axis=1, inplace=True)
+                df.drop('Url', axis=1, inplace=True)
+                df.to_csv(full_file_name)
+
+    def delete_first_3_rows(self):# "2-in-1_Laptops.csv",
+        files_to_delete_from = ["Battery_Life_Laptops.csv", "Business_Laptops.csv",
+                                "Laptops_for_College_Students.csv", "Laptops_for_Video_Editing.csv"]
+        for root, dir_names, file_names in os.walk(self.files_dir):
+            for file_name in file_names:
+                if file_name not in files_to_delete_from:
+                    continue
+                full_file_name = os.path.join(root, file_name)
+                df = pd.read_csv(full_file_name)
+                df.drop(index=[0, 1, 2], inplace=True)
+                df.to_csv(full_file_name)
 
 
 if __name__ == '__main__':
     dataFormatter = DataFormatter('resources')
-    dataFormatter.add_csv_ending()
-    dataFormatter.reformat_files()
+    dataFormatter.delete_first_2_columns()
 
